e3284f197536110c90a492cc43e1fda867395966
VioleRay <lirui9502@qq.com>
2021-07-06 11:30:31 +0800

    Initial commit

---
+++ src/main/java/com/javsm/GitTest.java
@@ -0,0 +1,19 @@
+package com.javsm;
+
+import org.junit.Test;
+
+/**
+ * @Author： lirui
+ * @Version： 1.0
+ * @Date： 2021/7/5-23:16
+ * @Since: jdk1.8
+ * @Description: TODO
+ */
+public class GitTest {
+
+    @Test
+    public void test(){
+        System.out.println("test1");
+        System.out.println("test2");
+    }
+}
